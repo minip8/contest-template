f703f1ce3a0722b49861ed2ae7c9a29ee5a48720
Matthew Yang <mattyang1340@gmail.com>
2026-09-24 09:16:49 +1000

    upd

--- template/hash_alt.sh
+++ template/hash_alt.sh
@@ -1,4 +1,1 @@
-#!/bin/sh
 sed 's/#.*//' | tr -d ' \n\r' | md5sum | cut -c-6
-# Hashes stdin, ignoring comments, spaces and newlines (tabs are kept).
-# Usage: ./hash.sh < Makefile        (or: cat Makefile | ./hash.sh)
